142e60e712378d4b8932bff49f619f088e525412
Andrew Cranford <acranford25@gmail.com>
2023-10-05 12:41:02 -0400

    deleted s from divs

--- client/src/components/Shop/ItemRow.jsx
+++ client/src/components/Shop/ItemRow.jsx
@@ -3,11 +3,11 @@ import AddToCart from "./AddToCart";
 
 export default function ItemRow({ item }) {
   return (
-    <div>
+    <divs>
       <img src={item.imagereel[0].image} alt="imageNotFound" />
       <p className="text-black">{item.name}</p>
       <p className="text-black">{item.description}</p>
       <p className="text-black">price: ${item.cost}</p>
-    </div>
+    </divs>
   );
 }
