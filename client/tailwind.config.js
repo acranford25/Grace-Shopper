02638ca7b018d604a4a6fd1f80db3fb91a618c28
Andrew Cranford <acranford25@gmail.com>
2023-10-09 10:54:39 -0400

    tailwind and css config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html, js, jsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{html, js, jsx}, ./components/**/*.{html, js, jsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        gridTemplateRows: {
+          layout: "10vh 80vh 10vh",
+        },
+        gridTemplateColumns: {
+          layout: "25% 75%",
+        },
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
