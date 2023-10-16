13d8e05d5f4f3c060ce90c0d0b731f6d709f1331
Andrew Cranford <acranford25@gmail.com>
2023-10-16 12:14:56 -0400

    added category sidebar to singleItem page

--- client/src/components/Shop/SingleItem.jsx
+++ client/src/components/Shop/SingleItem.jsx
@@ -3,6 +3,7 @@ import { fetchItem } from "../../api/items";
 import { useEffect, useState } from "react";
 import AddToCart from "./AddToCart";
 import "bootstrap/dist/css/bootstrap.min.css";
+import CategorySidebar from "./CategorySidebar";
 
 export default function SingleItem() {
   const { itemId } = useParams();
@@ -26,20 +27,23 @@ export default function SingleItem() {
   }, [setItem]);
 
   return (
-    <div className="container  tw-p-4 tw-m-4 tw-min-h-screen">
-      <div className="card tw-bg-base-100 tw-shadow-xl tw-flex tw-flex-row">
-        <figure>
-          <img
-            className="object-scale-down h-30 w-50"
-            src={image}
-            alt="imageNotFound"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title text-black">{item.name} </h2>
-          <p className="text-black">Description: {item.description} </p>
-          <p>Price: ${item.cost} </p>
-          <AddToCart item={item} />
+    <div className="tw-container tw-grid tw-grid-cols-4 tw-grid-rows-4 tw-gap-4 tw-pt-10 tw-pl-8">
+      <CategorySidebar />
+      <div className="container  tw-p-4 tw-m-4 tw-min-h-screen tw-col-span-3  tw-pl-8">
+        <div className="card tw-bg-base-100 tw-shadow-xl tw-flex tw-flex-row">
+          <figure>
+            <img
+              className="object-scale-down h-30 w-50"
+              src={image}
+              alt="imageNotFound"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title text-black">{item.name} </h2>
+            <p className="text-black">Description: {item.description} </p>
+            <p>Price: ${item.cost} </p>
+            <AddToCart item={item} />
+          </div>
         </div>
       </div>
     </div>
