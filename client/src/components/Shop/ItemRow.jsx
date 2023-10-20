2207de92a210291f7f7b1646245be8f41322e355
Andrew Cranford <acranford25@gmail.com>
2023-10-20 12:08:43 -0400

    corrected syntax error

--- client/src/components/Shop/ItemRow.jsx
+++ client/src/components/Shop/ItemRow.jsx
@@ -3,10 +3,10 @@ import AddToCart from "./AddToCart";
 
 export default function ItemRow({ item }) {
   return (
-    <divs>
+    <div>
       <img src={item.imagereel[0].image} alt="imageNotFound" />
       <p className="text-black">{item.name}</p>
       <h4 className="text-black">${item.cost}</h4>
-    </divs>
+    </div>
   );
 }
